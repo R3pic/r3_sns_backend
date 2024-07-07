4b442bf1b9b5e957f79d2978729f960c66cbcca3
r3pic <101osc@naver.com>
2024-07-07 17:37:13 +0900

    style: 404 반환시 url 표시

--- src/common/NotFoundHandler.ts
+++ src/common/NotFoundHandler.ts
@@ -2,7 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 import createHttpError from 'http-errors';
 
 const NotFoundHandler = (req: Request, res: Response, next: NextFunction) => {
-    const error = createHttpError(404, { name: 'Not Found', message: 'The requested resource could not be found' });
+    const url = req.url;
+    const error = createHttpError(404, { name: 'Not Found', message: `${url} not found` });
     next(error);
 }
 
