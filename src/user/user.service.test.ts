324ad0fa32e69ea229a8254ad3208abaa797196a
r3pic <101osc@naver.com>
2024-07-08 02:49:32 +0900

    test: user 테스트 코드 수정
    Spyon을 이용하게 함

--- src/user/user.service.test.ts
+++ src/user/user.service.test.ts
@@ -1,23 +1,11 @@
-import { describe, it, expect, jest, beforeEach } from "@jest/globals";
+import { describe, it, expect, jest } from "@jest/globals";
 import { UserService } from "./user.service";
 import createError from 'http-errors';
 import { UserRepository } from '../user/user.repository';
 
-jest.mock('../user/user.repository');
-
-type MockedUserRepository = jest.Mocked<UserRepository>;
+const userService = new UserService();
 
 describe('UserService (검증)', () => {
-    let userService: UserService;
-    let mockedUserRepository: MockedUserRepository;
-
-    beforeEach(() => {
-        mockedUserRepository = new UserRepository() as MockedUserRepository;
-        userService = new UserService();
-        (userService as any).userRepository = mockedUserRepository;
-        jest.clearAllMocks();
-    });
-
     describe('getUser', () => {
         it('getUser 함수가 존재해야 한다', () => {
             expect(userService.getUser).toBeDefined();
@@ -26,7 +14,7 @@ describe('UserService (검증)', () => {
         it('getUser 존재하지 않는 사용자', async () => {
             const userid = 'existingUser';
 
-            mockedUserRepository.findUserByUserId.mockResolvedValue(null);
+            jest.spyOn(UserRepository.prototype, 'findUserByUserId').mockResolvedValue(null);
 
             await expect(userService.getUser(userid)).rejects.toThrowError(
                 createError(404, { name: 'Not Found Error', message: 'User does not exist' })
@@ -36,7 +24,7 @@ describe('UserService (검증)', () => {
         it('getUser 존재하는 사용자', async () => {
             const userid = 'existingUser';
 
-            mockedUserRepository.findUserByUserId.mockResolvedValue({
+            jest.spyOn(UserRepository.prototype, 'findUserByUserId').mockResolvedValue({
                 id: 1,
                 email: 'test@gmail.com',
                 userid: 'existingUser',
