253e1dab4705c4beb1244f64987a6a6eccb471ee
r3pic <101osc@naver.com>
2024-07-08 02:59:39 +0900

    docs: UserController 엔드포인트 스웨거 작성
    기본 틀

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,15 +10,75 @@ export const userController = Router();
  * @swagger
  * /user/{userid}:
  *   get:
- *     summary: Get a user
+ *     summary: Get a user by ID
  *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: userid
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID
  *     responses:
- *      200:
- *       description: OK
- *      400:
- *       description: Bad request
- *      404:
- *       description: Not Found
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 nickname:
+ *                   type: string
+ *                 userid:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                   example: Bad Request
+ *                 status:
+ *                   type: number
+ *                   example: 400
+ *                 message:
+ *                   type: string
+ *                   example: Validation Error
+ *                 errors:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       field:
+ *                         type: string
+ *                         example: userid
+ *                       value:
+ *                         type: string
+ *                         example: ''
+ *                       reasons:
+ *                         type: array
+ *                         items:
+ *                          type: string
+ *                          example: 'userid should not be empty'
+ *       404:
+ *         description: Not Found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                   example: Not Found Error
+ *                 status:
+ *                   type: number
+ *                   example: 404
+ *                 message:
+ *                   type: string
+ *                   example: User does not exist
  */
 userController.get('/:userid', validateParams(GetUserParamsDto), async (req, res, next) => {
     const { userid } = req.params;
